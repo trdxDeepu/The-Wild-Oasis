f518aba0e2ca8d2c0ba4f7681648c859d1cdaba8
trdxlucifer <singhdepu566@gmail.com>
2023-09-23 12:28:42 +0530

    using styled component created heading componets as css

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,7 @@ import styled from "styled-components";
 import GlobalStyle from "./styles/GlobalStyle";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-
-const H1 = styled.h1`
-  font-size: 50px;
-  font-weight: 600;
-  background-color: aqua;
-`;
-
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.div`
   background-color: orangered;
@@ -19,18 +12,20 @@ const StyledApp = styled.div`
 function App() {
   return (
     <>
-    <GlobalStyle/>
-    <StyledApp>
-      <H1>The wild oasis</H1>
-
-      <Button onClick={() => alert("Check in")}>Check in</Button>
-      <Button onClick={() => alert("Check out")}>Check out</Button>
-
-      <Input type="number" placeholder="number of guest"/>
-      <Input type="number" placeholder=""/>
-    </StyledApp></>
-    
+      <GlobalStyle />
+      <StyledApp>
+        <Heading as="h1">The wild oasis</Heading>
+        
+        <Heading as="h2">check in check out</Heading>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onClick={() => alert("Check out")}>Check out</Button>
+        
+        <Heading as="h3">Form</Heading>
+        <Input type="number" placeholder="number of guest" />
+        <Input type="number" placeholder="" />
+      </StyledApp>
+    </>
   );
-} 
+}
 
 export default App;
---
+++ src/ui/Heading.jsx
@@ -0,0 +1,32 @@
+import styled, { css } from "styled-components";
+
+/* const test = css`
+  text-align: center;
+  ${10 > 5 && " background-color: yellow"}
+`; */
+
+const Heading = styled.h1`
+  ${(props) =>
+    props.as === "h1" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+      text-transform: uppercase;
+    `}
+  ${(props) =>
+    props.as === "h2" &&
+    css`
+      font-size: 2.5rem;
+      font-weight: 600;
+      text-transform: uppercase;
+    `}
+ ${(props) =>
+    props.as === "h3" &&
+    css`
+      font-size: 2rem;
+      font-weight: 600;
+      text-transfom: uppercase;
+    `}
+`;
+
+export default Heading;
