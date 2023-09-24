5a6906440fc8f5fd12b21fb308a4ceffeb8e9c97
trdxlucifer <singhdepu566@gmail.com>
2023-09-24 12:21:36 +0530

    installed supabase postgree sql database for backend in the project

---
+++ src/services/apiCabins.js
@@ -0,0 +1,11 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  let { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.log(error);
+    throw new Error("couldn't get data");
+  }
+  return data;
+}
