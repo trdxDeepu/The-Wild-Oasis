110daab6357da51a491c6e82c106414565078a9e
trdxlucifer <singhdepu566@gmail.com>
2023-09-29 01:02:05 +0530

    created booking component and adding pagination to the page of booking and guests to show the details

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -9,6 +9,9 @@ import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
 
 import { useMoveBack } from "../../hooks/useMoveBack";
+import { useBooking } from "./useBooking";
+import Empty from "../../ui/Empty";
+import Spinner from "../../ui/Spinner";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -17,10 +20,17 @@ const HeadingGroup = styled.div`
 `;
 
 function BookingDetail() {
-  const booking = {};
-  const status = "checked-in";
+
+ const{booking,isLoading} = useBooking()
 
   const moveBack = useMoveBack();
+
+  if (isLoading) return <Spinner />;
+  if (!booking) return <Empty resourceName="booking" />;
+
+  const { status, id: bookingId } = booking;
+
+
 
   const statusToTagName = {
     unconfirmed: "blue",
@@ -32,7 +42,7 @@ function BookingDetail() {
     <>
       <Row type="horizontal">
         <HeadingGroup>
-          <Heading as="h1">Booking #X</Heading>
+          <Heading as="h1">Booking #{bookingId}</Heading>
           <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
         </HeadingGroup>
         <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,21 +1,20 @@
-/* eslint-disable no-unused-vars */
-
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
-
   const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+  if (!cabins.length) return <Empty resourceName="cabins" />;
 
-  /* For Filters */
-  const filterValue = searchParams.get("discount");
+  // 1) FILTER
+  const filterValue = searchParams.get("discount") || "all";
 
   let filteredCabins;
   if (filterValue === "all") filteredCabins = cabins;
@@ -24,10 +23,8 @@ function CabinTable() {
   if (filterValue === "with-discount")
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
 
-  /* For Sorting */
-
+  // 2) SORT
   const sortBy = searchParams.get("sortBy") || "startDate-asc";
-
   const [field, direction] = sortBy.split("-");
   const modifier = direction === "asc" ? 1 : -1;
   const sortedCabins = filteredCabins.sort(
@@ -37,14 +34,15 @@ function CabinTable() {
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-        <Table.Header role="row">
+        <Table.Header>
           <div></div>
-          <div>Cabins</div>
+          <div>Cabin</div>
           <div>Capacity</div>
           <div>Price</div>
           <div>Discount</div>
           <div></div>
         </Table.Header>
+
         <Table.Body
           // data={cabins}
           // data={filteredCabins}
